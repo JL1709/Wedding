b4dc977514c2044683f42b6a32e3dfaf5f45a779
Lea Müller <lea.mueller@tuebingen.mpg.de>
2022-03-26 00:35:55 +0100

    first version julian and lea

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbzwakMvFC1v5kagak2HPfdSDpODP6vn7bUZ1E2DFYAd8ZSMotL2ONgDT9nuV_3mjHl7LA/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
